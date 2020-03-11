15f6903fc4df043b757fa27bfced156db8790e14
Nikola <anotherdevdzoni@gmail.com>
2020-03-11 12:37:25 +0100

    added a function;

--- helloworld.c
+++ helloworld.c
@@ -4,9 +4,19 @@ void printUserName(char *name) {
 	printf("%s\n", name);
 }
 
+int add(int x, int y) {
+	return x + y;
+}
+
 int main () {
 
 	printf("Hello world\n");
 	printf("Hello!\t\n");
+	
+	int a, b;
+	scanf("%d %d", &a, &b);
+	
+	printf("%d", add(a,b));
+	
 	return 0;
 }//done
